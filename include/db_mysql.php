1d9d6b9486e7f5b04387e12885ce50efe1cbae02
Alex <biggun@writeme.com>
2025-04-13 21:40:44 -0500

    PHP+MSQL V8 support

--- include/db_mysql.php
+++ include/db_mysql.php
@@ -1,5 +1,23 @@
 <?php
 
+// define("PRINT_QUERIES", 1);
+mysqli_report(MYSQLI_REPORT_STRICT | MYSQLI_REPORT_ALL);
+
+function to_string($value): string {
+	if (is_object($value)) {
+		return 'Object of class ' . get_class($value);
+	}
+	if (is_array($value)) {
+		return 'Array: ' . print_r($value, true); // or json_encode($value)
+	}
+	if (is_bool($value)) {
+		return $value ? 'true' : 'false';
+	}
+	if (is_null($value)) {
+		return 'null';
+	}
+	return (string)$value;
+}
 
 class Db {
   var $no_error = 0;
@@ -10,27 +28,31 @@ class Db {
   var $query_array = array();
   var $table_fields = array();
 
-  function Db($db_name, $db_host = NULL, $db_user = NULL, $db_password = NULL, $db_pconnect = 0) {
-    $connect_handle = ($db_pconnect) ? "mysql_pconnect" : "mysql_connect";
-    if (!$this->connection = $connect_handle($db_host, $db_user, $db_password)) {
-      $this->error("Could not connect to the database server ($db_host, $db_user).", 1);
-    }
-    if ($db_name != "") {
-      if (!@mysql_select_db($db_name)) {
-        @mysql_close($this->connection);
-        $this->error("Could not select database ($db_name).", 1);
+  function __construct($db_name, $db_host = NULL, $db_user = NULL, $db_password = NULL, $db_pconnect = 0) {
+    // $connect_handle = ($db_pconnect) ? "mysql_pconnect" : "mysql_connect";
+    // if (!$this->connection = $connect_handle($db_host, $db_user, $db_password)) {
+    try {
+      if (!$this->connection = mysqli_connect($db_host, $db_user, $db_password))
+        $this->error("Could not connect to the database server ($db_host, $db_user).", 1);
+      if ($db_name != "") {
+        if (!mysqli_select_db($this->connection, $db_name)) {
+          mysqli_close($this->connection);
+          $this->error("Could not select database ($db_name).", 1);
+        }
       }
+    } catch (mysqli_sql_exception $e) {
+      die("DB connection failed: " . $e->getMessage());
     }
-    // mysql_query("SET NAMES cp1251", $this->connection);
+    // mysqli_query($this->connection, "SET NAMES cp1251");
     return $this->connection;
   }
 
   function close() {
     if ($this->connection) {
       if ($this->query_id) {
-        @mysql_free_result($this->query_id);
+        mysqli_free_result($this->query_id);
       }
-      return @mysql_close($this->connection);
+      return mysqli_close($this->connection);
     }
     else {
       return false;
@@ -38,21 +60,24 @@ class Db {
   }
 
   function table_exists($table) {
-    if ($this->query("SHOW TABLES LIKE '$table';"))
-      if ($this->fetch_array())
-        return true;
+    if ($this->query("SHOW TABLES LIKE '$table';")) {
+        $num_rows = $this->get_numrows();
+        return $num_rows > 0;
+    }
     return false;
   }
 
   function query($query = "", $print_result = NULL) {
     unset($this->query_id);
+    // echo "query: $query<br>";
     if ($query != "") {
       if ((defined("PRINT_QUERIES") && PRINT_QUERIES == 1) || (defined("PRINT_STATS") && PRINT_STATS == 1)) {
         $startsqltime = explode(" ", microtime());
       }
-      if (!$this->query_id = @mysql_query($query, $this->connection)) {
-        $this->error("<b>Bad SQL Query</b>: ".htmlentities($query)."<br /><b>".mysql_error()."</b>");
+      if (!$this->query_id = mysqli_query($this->connection, $query)) {
+        $this->error("<b>Bad SQL Query</b>: ".htmlentities($query)."<br /><b>".mysqli_error($this->connection)."</b>");
       }
+      // echo "this->query_id: " . to_string($this->query_id) . "<br>";
       if ((defined("PRINT_QUERIES") && PRINT_QUERIES == 1) || (defined("PRINT_STATS") && PRINT_STATS == 1)) {
         $endsqltime = explode(" ", microtime());
         $totalsqltime = round($endsqltime[0]-$startsqltime[0]+$endsqltime[1]-$startsqltime[1],3);
@@ -71,28 +96,44 @@ class Db {
     }
   }
 
-  function fetch_array($query_id = -1, $assoc = 0) {
-    if ($query_id != -1) {
-      $this->query_id = $query_id;
+  function fetch_array($query_id = -1) {
+    if ($query_id != -1)
+        $this->query_id = $query_id;
+    if ($this->query_id)
+        return mysqli_fetch_array($this->query_id);
+    return NULL;
+  }
+  
+  function fetch_assoc($query_id = -1) {
+    // echo "query_id: $query_id<br>";
+    if ($query_id != -1)
+        $this->query_id = $query_id;
+    # if quiery
+    if ($this->query_id) {
+      if ($this->query_id instanceof mysqli_result) {
+        // echo "fetch_assoc: ";
+        // print_r($this->query_id);
+        $r = mysqli_fetch_assoc($this->query_id);
+        return $r;
+      }
+      else {
+        return $this->query_id;
+      }
     }
-    if ($this->query_id && is_resource($this->query_id)) {
-      return ($assoc) ? mysql_fetch_assoc($this->query_id) : mysql_fetch_array($this->query_id);
-    }
-    else
-      return $this->query_id;
+    return NULL;
   }
 
   function free_result($query_id = -1) {
     if ($query_id != -1) {
       $this->query_id = $query_id;
     }
-    return @mysql_free_result($this->query_id);
+    return mysqli_free_result($this->query_id);
   }
 
   function print_result($ar = NULL) {
     if ($this->query_id)
     {
-      $result = $ar ? $ar : $this->fetch_array($this->query_id);
+      $result = $ar ? $ar : $this->fetch_assoc();
       echo "<b>Query result</b>:<br>";
       print_r($result);
       echo "<br>";
@@ -106,13 +147,11 @@ class Db {
   
   function query_firstrow($query = "", $print_result = false) {
     if ($query != "") {
-      $this->query($query);
+      $this->query($query, $print_result);
     }
     if (!$this->query_id)
       return NULL;
-    $result = $this->fetch_array($this->query_id);
-    if ($print_result)
-      $this->print_result($result);
+    $result = $this->fetch_assoc();
     $this->free_result();
     return $result;
   }
@@ -121,11 +160,11 @@ class Db {
     if ($query_id != -1) {
       $this->query_id = $query_id;
     }
-    return mysql_num_rows($this->query_id);
+    return mysqli_num_rows($this->query_id);
   }
 
   function get_insert_id() {
-    return ($this->connection) ? @mysql_insert_id($this->connection) : 0;
+    return ($this->connection) ? mysqli_insert_id($this->connection) : 0;
   }
 
   function get_next_id($column = "", $table = "") {
@@ -144,39 +183,42 @@ class Db {
     if ($query_id != -1) {
       $this->query_id = $query_id;
     }
-    return @mysql_num_fields($this->query_id);
+    return mysqli_num_fields($this->query_id);
   }
 
-  function get_fieldname($query_id = -1, $offset) {
+  function get_fieldinfo($query_id = -1, $offset = null) {
     if ($query_id != -1) {
       $this->query_id = $query_id;
     }
-    return @mysql_field_name($this->query_id, $offset);
+    return mysqli_fetch_field_direct($this->query_id, $offset);
+  }
+  
+  function get_fieldname($query_id = -1, $offset = null) {
+    $finfo = get_fieldinfo($query_id, $offset);
+    return $info->name;
   }
 
-  function get_fieldtype($query_id = -1, $offset) {
-    if ($query_id != -1) {
-      $this->query_id = $query_id;
-    }
-    return @mysql_field_type($this->query_id, $offset);
+  function get_fieldtype($query_id = -1, $offset = null) {
+    $finfo = get_fieldinfo($query_id, $offset);
+    return $info->type;
   }
 
   function affected_rows() {
-    return ($this->connection) ? @mysql_affected_rows($this->connection) : 0;
+    return ($this->connection) ? mysqli_affected_rows($this->connection) : 0;
   }
 
   function is_empty($query = "") {
     if ($query != "") {
       $this->query($query);
     }
-    return (!mysql_num_rows($this->query_id)) ? 1 : 0;
+    return (!mysqli_num_rows($this->query_id)) ? 1 : 0;
   }
 
   function not_empty($query = "") {
     if ($query != "") {
       $this->query($query);
     }
-    return (!mysql_num_rows($this->query_id)) ? 0 : 1;
+    return (!mysqli_num_rows($this->query_id)) ? 0 : 1;
   }
 
   function get_table_fields($table) {
@@ -185,7 +227,7 @@ class Db {
     }
     $this->table_fields[$table] = array();
     $result = $this->query("SHOW FIELDS FROM $table");
-    while ($row = $this->fetch_array($result)) {
+    while ($row = $this->fetch_assoc()) {
       $this->table_fields[$table][$row['Field']] = $row['Type'];
     }
     return $this->table_fields[$table];
